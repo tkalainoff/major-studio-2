630c7595e45aa375d753de25d53bd26420bfba8f
tkalainoff <t.kalainoff@gmail.com>
2023-05-13 12:00:35 -0400

    binded 3d discs to data

--- threeScene-testing/threeScene.js
+++ threeScene-testing/threeScene.js
@@ -24,68 +24,702 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // }
 
 // data
+// let rawData_old =[
+//   [ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
+//     {year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//     {year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+//   ],
+//   [ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+//     {year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//     {year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+//   ],
+//   [ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//     {year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+//     {year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+//   ],
+//   [ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
+//     {year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//     {year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+//   ],
+//   [ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//     {year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//     {year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
+//   ],
+//   [ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
+//   {year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//   {year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+// ],
+// [ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+//   {year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//   {year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+// ],
+// [ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//   {year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+//   {year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+// ],
+// [ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
+//   {year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//   {year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+// ],
+// [ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+//   {year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+//   {year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
+// ],
+// [ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
+// {year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+// {year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+// ],
+// [ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+// {year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+// {year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+// ],
+// [ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+// {year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
+// {year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
+// ],
+// [ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
+// {year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+// {year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
+// ],
+// [ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
+// {year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
+// {year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
+// ]
+// ];
+
 let rawData =[
-  [ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
-    {year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-    {year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-  ],
-  [ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-    {year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-    {year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-  ],
-  [ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-    {year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-    {year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-  ],
-  [ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
-    {year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-    {year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-  ],
-  [ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-    {year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-    {year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
-  ],
-  [ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
-  {year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-  {year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-],
-[ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-  {year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-  {year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-],
-[ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-  {year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-  {year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-],
-[ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
-  {year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-  {year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-],
-[ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-  {year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-  {year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
-],
-[ {year:2023,type:"multiracial",radiusTop:10,radiusBottom:10,color:"black"},
-{year:2023,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-{year:2023,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-],
-[ {year:2022,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-{year:2022,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-{year:2022,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-],
-[ {year:2021,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-{year:2021,type:"multiracial",radiusTop:7,radiusBottom:7,color:"black"},
-{year:2021,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"}
-],
-[ {year:2020,type:"multiracial",radiusTop:5,radiusBottom:5,color:"black"},
-{year:2020,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-{year:2020,type:"white",radiusTop:100,radiusBottom:100,color:"white"}
-],
-[ {year:2019,type:"poc",radiusTop:25,radiusBottom:25,color:"Fuchsia"},
-{year:2019,type:"white",radiusTop:100,radiusBottom:100,color:"white"},
-{year:2019,type:"multiracial",radiusTop:2,radiusBottom:2,color:"black"}
-]
+  [{
+    year: 1993,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1993,
+    type: "poc",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "Fuchsia"
+},
+{
+    year: 1993,
+    type: "white",
+    radiusTop: 9,
+    radiusBottom: 9,
+    color: "white"
+}],
+[{
+    year: 1994,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1994,
+    type: "poc",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "Fuchsia"
+},
+{
+    year: 1994,
+    type: "white",
+    radiusTop: 4,
+    radiusBottom: 4,
+    color: "white"
+}],
+[{
+    year: 1995,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1995,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 1995,
+    type: "white",
+    radiusTop: 8,
+    radiusBottom: 8,
+    color: "white"
+}],
+[{
+    year: 1996,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1996,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 1996,
+    type: "white",
+    radiusTop: 8,
+    radiusBottom: 8,
+    color: "white"
+}],
+[{
+    year: 1997,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1997,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 1997,
+    type: "white",
+    radiusTop: 13,
+    radiusBottom: 13,
+    color: "white"
+}],
+[{
+    year: 1998,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1998,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 1998,
+    type: "white",
+    radiusTop: 8,
+    radiusBottom: 8,
+    color: "white"
+}],
+[{
+    year: 1999,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 1999,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 1999,
+    type: "white",
+    radiusTop: 14,
+    radiusBottom: 14,
+    color: "white"
+}],
+  [{
+    year: 2000,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2000,
+    type: "poc",
+    radiusTop: 3,
+    radiusBottom: 3,
+    color: "Fuchsia"
+},
+{
+    year: 2000,
+    type: "white",
+    radiusTop: 7,
+    radiusBottom: 7,
+    color: "white"
+}],
+[{
+    year: 2001,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2001,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 2001,
+    type: "white",
+    radiusTop: 15,
+    radiusBottom: 15,
+    color: "white"
+}],
+[{
+    year: 2002,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2002,
+    type: "poc",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "Fuchsia"
+},
+{
+    year: 2002,
+    type: "white",
+    radiusTop: 11,
+    radiusBottom: 11,
+    color: "white"
+}],
+[{
+    year: 2003,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2003,
+    type: "poc",
+    radiusTop: 4,
+    radiusBottom: 4,
+    color: "Fuchsia"
+},
+{
+    year: 2003,
+    type: "white",
+    radiusTop: 20,
+    radiusBottom: 20,
+    color: "white"
+}],
+[{
+    year: 2004,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2004,
+    type: "poc",
+    radiusTop: 5,
+    radiusBottom: 5,
+    color: "Fuchsia"
+},
+{
+    year: 2004,
+    type: "white",
+    radiusTop: 17,
+    radiusBottom: 17,
+    color: "white"
+}],
+[{
+    year: 2005,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2005,
+    type: "poc",
+    radiusTop: 3,
+    radiusBottom: 3,
+    color: "Fuchsia"
+},
+{
+    year: 2005,
+    type: "white",
+    radiusTop: 21,
+    radiusBottom: 21,
+    color: "white"
+}],
+  [{
+    year: 2006,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2006,
+    type: "poc",
+    radiusTop: 4,
+    radiusBottom: 4,
+    color: "Fuchsia"
+},
+{
+    year: 2006,
+    type: "white",
+    radiusTop: 17,
+    radiusBottom: 17,
+    color: "white"
+}],
+[{
+    year: 2007,
+    type: "mixed",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "black"
+},
+{
+    year: 2007,
+    type: "poc",
+    radiusTop: 3,
+    radiusBottom: 3,
+    color: "Fuchsia"
+},
+{
+    year: 2007,
+    type: "white",
+    radiusTop: 20,
+    radiusBottom: 20,
+    color: "white"
+}],
+[{
+    year: 2008,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2008,
+    type: "poc",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "Fuchsia"
+},
+{
+    year: 2008,
+    type: "white",
+    radiusTop: 17,
+    radiusBottom: 17,
+    color: "white"
+}],
+[{
+    year: 2009,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2009,
+    type: "poc",
+    radiusTop: 5,
+    radiusBottom: 5,
+    color: "Fuchsia"
+},
+{
+    year: 2009,
+    type: "white",
+    radiusTop: 31,
+    radiusBottom: 31,
+    color: "white"
+}],
+[{
+    year: 2010,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2010,
+    type: "poc",
+    radiusTop: 3,
+    radiusBottom: 3,
+    color: "Fuchsia"
+},
+{
+    year: 2010,
+    type: "white",
+    radiusTop: 27,
+    radiusBottom: 27,
+    color: "white"
+}],
+[{
+    year: 2011,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2011,
+    type: "poc",
+    radiusTop: 7,
+    radiusBottom: 7,
+    color: "Fuchsia"
+},
+{
+    year: 2011,
+    type: "white",
+    radiusTop: 44,
+    radiusBottom: 44,
+    color: "white"
+}],
+[{
+    year: 2012,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2012,
+    type: "poc",
+    radiusTop: 10,
+    radiusBottom: 10,
+    color: "Fuchsia"
+},
+{
+    year: 2012,
+    type: "white",
+    radiusTop: 28,
+    radiusBottom: 28,
+    color: "white"
+}],
+[{
+    year: 2013,
+    type: "mixed",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "black"
+},
+{
+    year: 2013,
+    type: "poc",
+    radiusTop: 3,
+    radiusBottom: 3,
+    color: "Fuchsia"
+},
+{
+    year: 2013,
+    type: "white",
+    radiusTop: 41,
+    radiusBottom: 41,
+    color: "white"
+}],
+[{
+    year: 2014,
+    type: "mixed",
+    radiusTop: 0,
+    radiusBottom: 0,
+    color: "black"
+},
+{
+    year: 2014,
+    type: "poc",
+    radiusTop: 15,
+    radiusBottom: 15,
+    color: "Fuchsia"
+},
+{
+    year: 2014,
+    type: "white",
+    radiusTop: 45,
+    radiusBottom: 45,
+    color: "white"
+}],
+[{
+    year: 2015,
+    type: "mixed",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "black"
+},
+{
+    year: 2015,
+    type: "poc",
+    radiusTop: 14,
+    radiusBottom: 14,
+    color: "Fuchsia"
+},
+{
+    year: 2015,
+    type: "white",
+    radiusTop: 41,
+    radiusBottom: 41,
+    color: "white"
+}],
+[{
+    year: 2016,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2016,
+    type: "poc",
+    radiusTop: 13,
+    radiusBottom: 13,
+    color: "Fuchsia"
+},
+{
+    year: 2016,
+    type: "white",
+    radiusTop: 40,
+    radiusBottom: 40,
+    color: "white"
+}],
+[{
+    year: 2017,
+    type: "mixed",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "black"
+},
+{
+    year: 2017,
+    type: "poc",
+    radiusTop: 10,
+    radiusBottom: 10,
+    color: "Fuchsia"
+},
+{
+    year: 2017,
+    type: "white",
+    radiusTop: 46,
+    radiusBottom: 46,
+    color: "white"
+}],
+[{
+    year: 2018,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2018,
+    type: "poc",
+    radiusTop: 16,
+    radiusBottom: 16,
+    color: "Fuchsia"
+},
+{
+    year: 2018,
+    type: "white",
+    radiusTop: 37,
+    radiusBottom: 37,
+    color: "white"
+}],
+[{
+    year: 2019,
+    type: "mixed",
+    radiusTop: 1,
+    radiusBottom: 1,
+    color: "black"
+},
+{
+    year: 2019,
+    type: "poc",
+    radiusTop: 16,
+    radiusBottom: 16,
+    color: "Fuchsia"
+},
+{
+    year: 2019,
+    type: "white",
+    radiusTop: 39,
+    radiusBottom: 39,
+    color: "white"
+}],
+[{
+    year: 2020,
+    type: "mixed",
+    radiusTop: 4,
+    radiusBottom: 4,
+    color: "black"
+},
+{
+    year: 2020,
+    type: "poc",
+    radiusTop: 22,
+    radiusBottom: 22,
+    color: "Fuchsia"
+},
+{
+    year: 2020,
+    type: "white",
+    radiusTop: 37,
+    radiusBottom: 37,
+    color: "white"
+}],
+[{
+    year: 2021,
+    type: "mixed",
+    radiusTop: 2,
+    radiusBottom: 2,
+    color: "black"
+},
+{
+    year: 2021,
+    type: "poc",
+    radiusTop: 20,
+    radiusBottom: 20,
+    color: "Fuchsia"
+},
+{
+    year: 2021,
+    type: "white",
+    radiusTop: 30,
+    radiusBottom: 30,
+    color: "white"
+}],
+[{
+      year: 2022,
+      type: "mixed",
+      radiusTop: 2,
+      radiusBottom: 2,
+      color: "black"
+  },
+  {
+      year: 2022,
+      type: "poc",
+      radiusTop: 12,
+      radiusBottom: 12,
+      color: "Fuchsia"
+  },
+  {
+      year: 2022,
+      type: "white",
+      radiusTop: 45,
+      radiusBottom: 45,
+      color: "white"
+  }]
 ];
+
 // let discData = d3.groups(rawData, d => d.year)
 // console.log(discData)
 
@@ -161,7 +795,7 @@ let rawData =[
     }
   
     function createGeometries(params) {
-        const disc = new THREE.CylinderGeometry(params, params, 5, 64 );  
+        const disc = new THREE.CylinderGeometry(params, params, 3, 64 );  
         return {
             disc
         }
@@ -182,7 +816,7 @@ let rawData =[
         
         const meshes = year.map((yearObjects, j) => { // loop through each cylinder (3)
           
-          yOffset = i * 15 - j + 25 // adjust offset
+          yOffset = i * 7 - j + 20 // adjust offset
           const discGroup = new THREE.Group(); // create disc group for each year
           // geometries
           geometries = createGeometries(yearObjects["radiusTop"]);
